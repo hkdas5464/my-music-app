57d3a014e7d3df28912b73d2aecab8feb7d3a34d
Harendra Kumar Das <63789292+hkdas5464@users.noreply.github.com>
2021-05-24 09:57:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import './App.css';
-import Pages from './pages/SendingData';
-import { Route, Switch,Link } from 'react-router-dom';
-import FetchData from './pages/fetchingData';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import WhatsAppIcon from '@material-ui/icons/WhatsApp';
-import GitHubIcon from '@material-ui/icons/GitHub';
-
+import "./App.css";
+import Pages from "./pages/SendingData";
+import { Route, Switch } from "react-router-dom";
+import FetchData from "./pages/fetchingData";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 function App() {
   return (
     <Route>
-    <div className="App">
-     
-          <Switch>
-        
-        <Route path="/upload" >
-         <Pages/>
-        </Route>
-        <Route path="/">
-
-        <FetchData/>
-        </Route>
-        
-      </Switch>
-          </div>
-          <footer className="footer">
-            <hr className="hr"/>
+      <div className="App">
+        <Switch>
+          <Route path="/upload">
+            <Pages />
+          </Route>
+          <Route path="/">
+            <FetchData />
+          </Route>
+        </Switch>
+      </div>
+      <footer className="footer">
+        <hr className="hr" />
         <p>Created By Harendra Kumar Das</p>
         <div className="col item social">
-          <Link to="https://www.facebook.com/harendrakumar.harendrakumar.5876" style={{paddingLeft: 13, textDecoration: 'none'}}><FacebookIcon className="fb"/></Link>
-        <Link to="https://github.com/hkdas5464"><GitHubIcon className="fb" style={{ color: "black" }} /></Link>
-         <Link to="whatsapp://send?text=http://8789369732"><WhatsAppIcon className="fb" style={{ color: "green" }}/></Link> 
-          <Link to="mailto:someone@example.com"><MailOutlineIcon className="fb" style={{ color: "red" }}/></Link>
-          </div>
-    </footer>
+          <a
+            href="https://www.facebook.com/harendrakumar.harendrakumar.5876"
+            style={{ paddingLeft: 13, textDecoration: "none" }}
+          >
+            <FacebookIcon className="fb" />
+          </a>
+          <a
+            href="https://github.com/hkdas5464"
+            style={{ paddingLeft: 13, textDecoration: "none" }}
+          >
+            <GitHubIcon className="fb" style={{ color: "black" }} />
+          </a>
+          <a
+            href="https://wa.me/8789369732"
+            style={{ paddingLeft: 13, textDecoration: "none" }}
+          >
+            <WhatsAppIcon className="fb" style={{ color: "green" }} />
+          </a>
+
+          <a href="mailto:someone@example.com">
+            <MailOutlineIcon className="fb" style={{ color: "red" }} />
+          </a>
+        </div>
+      </footer>
     </Route>
-     
   );
 }
 
